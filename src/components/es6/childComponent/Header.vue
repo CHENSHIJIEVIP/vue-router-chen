1dbe0af865fca351829f29ad7d28d890a884d742
chenshijie <jason_chenshijie@sina.com>
2017-12-22 15:43:41 +0800

    es6组件的子组件

---
+++ src/components/es6/childComponent/Header.vue
@@ -0,0 +1,26 @@
+<template>
+	<header class="header">
+		<div id="logo">{{logo}}</div>
+		<ul class="nav">
+			<li v-for="item in navs">{{item.li}}</li>
+		</ul>
+	</header>
+</template>
+<script>
+	export default{
+		name:'header',
+		data(){
+			return{
+				navs:[
+					{li:'主页'},
+					{li:'日志'},
+					{li:'说说'},
+					{li:'相册'},
+					{li:'其他'}
+				]
+			}
+		},
+		props:['logo']
+	}
+</script>
+<style></style>
